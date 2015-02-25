88b1bf254b8589e6971795fe7ee78da606f0d916
Raizunne <andres_sa.97@hotmail.com>
2015-02-25 16:39:58 -0600

    v1.1.1 release

--- src/main/java/com/raizunne/redstonic/Redstonic.java
+++ src/main/java/com/raizunne/redstonic/Redstonic.java
@@ -28,7 +28,7 @@ import net.minecraftforge.common.MinecraftForge;
 public class Redstonic {
 
     public static final String MODID = "Redstonic";
-    public static final String VERSION = "1.1";
+    public static final String VERSION = "1.1.1";
 
     @Mod.Instance
     public static Redstonic instance;
